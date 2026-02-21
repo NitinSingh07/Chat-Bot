a18aeccf2f807c617c53447a282400f6bd739504
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2026-02-21 16:07:30 +0530

    feat: Add Clerk authentication configuration for Convex.

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://coherent-hare-22.clerk.accounts.dev",
+            domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
             applicationID: "convex",
         },
     ],
